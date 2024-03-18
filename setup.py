f00dcc7b772b5026049061509306d6ad9acfbe80
Ashish Mishragit config --global user.email ds.ashishmishra@gmail.com <ds.ashishmishra@gmail.com>
2024-03-19 01:57:19 +0530

    loggin and exception

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='score_predictor',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Ashish',
+author_email='ds.ashishmishra@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
